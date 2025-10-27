ea970f90817654596827bb47180818c1b6f43da1
Murylo fabichaki Lourenço <murylofabichakil0808@gmail.com>
2025-10-27 20:17:16 +0000

    menu lateral atualizado

--- assets/scripts/menu_lateral.js
+++ assets/scripts/menu_lateral.js
@@ -1,1 +1,8 @@
-let verificacao = sessionStorage.getItem("verificacao");
+let verificacao = localStorage.getItem("verificacao");
+if (verificacao == "certo") {
+  document.getElementById("certo").style.display = "block";
+  document.getElementById("errado").style.display = "none";
+} else {
+  document.getElementById("certo").style.display = "none";
+  document.getElementById("errado").style.display = "block";
+}
