4bb24c708d120906281bbf547bb871a0cbe60f67
Murylo fabichaki Lourenço <murylofabichakil0808@gmail.com>
2025-10-31 20:20:43 +0000

    cadastro atualizado

--- assets/scripts/cadastro.js
+++ assets/scripts/cadastro.js
@@ -1,24 +1,23 @@
 document.querySelector("form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    let nome_comnpleto = document.getElementById("nome-comnpleto").value;
-    let data_de_nascimeno = document.getElementById("data-de-nascimento").value;
-    let e_mail = document.getElementById("e-mail").value;
-    let telefone = document.getElementById("telefone").value;
-    let matricula = document.getElementById("matricula").value;
-    let senha = document.getElementById("senha").value;
-  
-    fetch("/api/cadastro", {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: `{nome_comnpleto:${nome_comnpleto},data_de_nascimeno:${data_de_nascimeno},e_mail:${e_mail},telefone:${telefone},matricula:${matricula},senha:${senha}}`,
-    }).then((res) => {
-      if (res.ok) {
-        window.location.href = "/pages/principal.html";
-      } else {
-        alert("Algo eatá errado!");
-      }
-    });
+  e.preventDefault();
+  let nome_comnpleto = document.getElementById("nome-comnpleto").value;
+  let data_de_nascimeno = document.getElementById("data-de-nascimento").value;
+  let e_mail = document.getElementById("e-mail").value;
+  let telefone = document.getElementById("telefone").value;
+  let matricula = document.getElementById("matricula").value;
+  let senha = document.getElementById("senha").value;
+
+  fetch("/api/cadastro", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: `{nomeCompleto:${nome_comnpleto},dataDeNasciment:${data_de_nascimeno},eMail:${e_mail},telefone:${telefone},matricula:${matricula},senha:${senha}}`,
+  }).then((res) => {
+    if (res.ok) {
+      window.location.href = "/pages/principal.html";
+    } else {
+      alert("Algo eatá errado!");
+    }
   });
-  
+});
