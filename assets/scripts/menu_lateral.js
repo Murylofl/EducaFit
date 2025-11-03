04a40d4df4f5a88e3c35b00251da76652a00e325
matheus240309 <99844941+Matheusfranx@users.noreply.github.com>
2025-11-03 20:12:40 +0000

    menu lateral atualizado

--- assets/scripts/menu_lateral.js
+++ assets/scripts/menu_lateral.js
@@ -6,3 +6,19 @@ if (verificacao == "certo") {
   document.getElementById("certo").style.display = "none";
   document.getElementById("errado").style.display = "block";
 }
+
+fetch("https://s3ddht-3000.csb.app/api/aluno", {
+  headers: {
+    Authenticate: "Bearer " + sessionStorage.getItem("token"),
+  },
+}).then(async (res) => {
+  const dados = await res.json();
+
+  console.log(dados);
+
+  document.getElementById("nome").innerText = dados.nomeCompleto;
+  document.getElementById("data_nasc").innerText = dados.dataDeNasciment;
+  document.getElementById("matricula").innerText = dados.matricula;
+  document.getElementById("telefone").innerText = dados.telefone;
+  document.getElementById("email").innerText = dados.eMail;
+});
