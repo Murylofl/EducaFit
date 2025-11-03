e9ff1ac39c6df1c2e14106331103756bcf34ad54
matheus240309 <99844941+Matheusfranx@users.noreply.github.com>
2025-11-03 20:12:20 +0000

    conta atualizado

---
+++ assets/scripts/conta.js
@@ -0,0 +1,15 @@
+fetch("https://s3ddht-3000.csb.app/api/aluno", {
+  headers: {
+    Authenticate: "Bearer " + sessionStorage.getItem("token"),
+  },
+}).then(async (res) => {
+  const dados = await res.json();
+
+  console.log(dados);
+
+  document.getElementById("nome").innerText = dados.nomeCompleto;
+  document.getElementById("data_nasc").innerText = dados.dataDeNasciment;
+  document.getElementById("matricula").innerText = dados.matricula;
+  document.getElementById("telefone").innerText = dados.telefone;
+  document.getElementById("email").innerText = dados.eMail;
+});
