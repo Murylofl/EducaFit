6dca356743ee836cd20d033b50227c07c5ff5db8
matheus240309 <99844941+Matheusfranx@users.noreply.github.com>
2025-11-03 20:11:54 +0000

    cadastro

--- assets/scripts/cadastro.js
+++ assets/scripts/cadastro.js
@@ -1,23 +1,40 @@
 document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault();
-  let nome_comnpleto = document.getElementById("nome-comnpleto").value;
+  let nome_completo = document.getElementById("nome-completo").value;
   let data_de_nascimeno = document.getElementById("data-de-nascimento").value;
   let e_mail = document.getElementById("e-mail").value;
   let telefone = document.getElementById("telefone").value;
   let matricula = document.getElementById("matricula").value;
   let senha = document.getElementById("senha").value;
+  console.log({
+    nome_completo,
+    data_de_nascimeno,
+    e_mail,
+    telefone,
+    senha,
+    matricula,
+  });
 
-  fetch("/api/cadastro", {
+  fetch("https://s3ddht-3000.csb.app/api/cadastro", {
     method: "post",
     headers: {
       "Content-Type": "application/json",
     },
-    body: `{nomeCompleto:${nome_comnpleto},dataDeNasciment:${data_de_nascimeno},eMail:${e_mail},telefone:${telefone},matricula:${matricula},senha:${senha}}`,
-  }).then((res) => {
+    body: JSON.stringify({
+      nomeCompleto: nome_completo,
+      dataDeNasciment: data_de_nascimeno,
+      eMail: e_mail,
+      telefone: telefone,
+      matricula: matricula,
+      senha: senha,
+    }),
+  }).then(async (res) => {
     if (res.ok) {
       window.location.href = "/pages/principal.html";
     } else {
-      alert("Algo eatá errado!");
+      console.log(res);
+      console.log(await res.text());
+      alert("Algo está errado!");
     }
   });
 });
