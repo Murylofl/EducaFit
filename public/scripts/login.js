0e81f1976b09dc3f9c45b20fa8a3b126eee0df77
Murylo fabichaki Lourenço <murylofabichakil0808@gmail.com>
2025-11-28 18:48:00 +0000

    brazil novo todo dia

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,3 +1,20 @@
+let borda = document.querySelector("#inputSenha");
+function mostrarSenha() {
+  let borda = document.querySelector("#inputSenha");
+  let aberto = document.querySelector("#aberto");
+  let fechado = document.querySelector("#fechado");
+  let input = document.querySelector("#senha");
+
+  if (aberto.style.display == "block" || aberto.style.display == "") {
+    aberto.style.display = "none";
+    fechado.style.display = "block";
+    input.type = "text";
+  } else {
+    aberto.style.display = "block";
+    fechado.style.display = "none";
+    input.type = "password";
+  }
+}
 document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault();
   console.log("fazendo login");
@@ -24,6 +41,8 @@ document.querySelector("form").addEventListener("submit", (e) => {
           for (coisa_2 in objetos) {
             document.getElementById(coisa_2).value = "";
             document.getElementById(coisa_2).style.borderColor = "red";
+            borda.style.border = "solid red";
+            document.getElementById(coisa_2).style.borderColor = "red";
             document
               .getElementById(coisa_2)
               .style.setProperty("--ph-color", "red");
@@ -32,8 +51,10 @@ document.querySelector("form").addEventListener("submit", (e) => {
           }
         } else {
           for (coisa_2 in objetos) {
+            console.log("matheuschato");
             document.getElementById(coisa_2).value = "";
             document.getElementById(coisa_2).style.borderColor = "red";
+            borda.style.border = "solid red";
             document
               .getElementById(coisa_2)
               .style.setProperty("--ph-color", "red");
